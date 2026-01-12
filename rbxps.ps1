def50ff7d0dafc02ab9b1be50d5a971cf582306d
itzpxn4 <99605757+itzpxn4@users.noreply.github.com>
2026-01-13 00:28:13 +0300

    added config system

--- rbxps.ps1
+++ rbxps.ps1
@@ -1,35 +1,35 @@
+$cfgdir = "$home\.config\rbxps"
+
+if (-not (test-path "$cfgdir\cfg.json"))
+    {
+    mkdir $cfgdir | Out-null
+    new-item -path "$cfgdir\cfg.json" | Out-Null
+    [PSCustomObject]@{items = @(@{ place = "Multicrew Tank Combat"; id = 95721658376580 })} | ConvertTo-Json | Set-Content -Path "$cfgdir\cfg.json" -Encoding utf8
+    }
+
+$cfg = Get-Content -Path "$cfgdir\cfg.json" -Raw | ConvertFrom-Json
+
 while ($true) {
     Clear-Host
     Write-Host "=== Roblox PowerShell ===" -ForegroundColor Cyan
     Write-Host 
-    Write-Host "1 Phantom Forces"
-    Write-Host "2 Scorched Earth"
-    Write-Host "3 Multicrew Tank Combat"
-    Write-Host 
-    Write-Host "Q Exit"
-    Write-Host
-
-    $choice = Read-Host " "
-
-    switch ($choice) {
-        "1" {
-            Start-Process "roblox://placeId=292439477"
-            exit
-        }
-        "2" {
-            Start-Process "roblox://placeId=13794093709"
-            exit
-        }
-        "3" {
-            Start-Process "roblox://placeId=95721658376580"
-            exit
-        }
-        "Q" {
-            exit
-        }
-        Default {
-            Write-Host "Error, try 1-3 or Q" -ForegroundColor Red
-            Start-Sleep -Seconds 1
-        }
+    for ($i = 0; $i -lt $cfg.items.Count; $i++) {
+    Write-Host "$($i+1) $($cfg.items[$i].place)"
     }
+    Write-Host "Q Exit"
+    Write-Host 
+    Write-Host ">" -NoNewline
+
+    $choice = Read-Host
+
+    if ($choice -ne "q") {
+        if ($choice -match "^\d+$" -and [int]$choice -lt $cfg.items.Count+1) {
+            Write-Host "Starting $($cfg.items[$choice-1].place)"
+            Start-Process "roblox://placeId=$($cfg.items[$choice-1].id)"
+            exit
+            }
+    }
+    else {
+        exit
+        }
 }
